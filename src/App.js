2e41d0dd0ea461550efd061c8e054bce238e8665
Aleem Nasiyathulla <aleemnasiyathulla2020@gmail.com>
2023-10-14 09:55:28 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Play from "./container/Play/Play";
-import Story from "./components/Story/Story";
+
 import Result from "./components/Result/Result";
 import Footer from "./components/Footer/Footer";
 
@@ -15,7 +15,6 @@ function App() {
     
           <Route path="/" element={<Play />} />
           <Route path="/result" element={<Result />} />
-          <Route path="*" element={<Story />} />
         </Routes>
         <Footer />
       </Router>
